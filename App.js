e26b8e78c2de5958c7981b31863207e2f40edd9d
Sơn Hoàng <60213798+SonHoang2@users.noreply.github.com>
2024-11-02 14:13:31 +0700

    done basic equation

--- App.js
+++ App.js
@@ -1,9 +1,45 @@
 import { SafeAreaView, View, StyleSheet, TextInput } from 'react-native';
 import Button from './components/Button';
 import React, { useState } from 'react';
+import { evaluate, log } from 'mathjs';
 
 export default function App() {
     const [input, setInput] = useState("");
+
+    const addOperator = (op) => {
+        let exp = input;
+        const last = exp.charAt(exp.length - 1);
+        if (last === "*" || last === "+" || last === "-" || last === "/") {
+            exp = exp.slice(0, -1) + op;
+            setInput(exp);
+        } else {
+            exp = exp + op;
+            setInput(exp);
+        }
+    };
+
+    const caculate = () => {
+        const inputString = input.toString();
+        
+        if (inputString === "") {
+            return;
+        }
+
+        const last = inputString.charAt(input.length - 1);
+
+        if (
+            last === "/" ||
+            last === "+" ||
+            last === "-" ||
+            last == "*" ||
+            last == "."
+        ) {
+            inputString = inputString.slice(0, inputString.length - 1);
+        }
+        
+        const result = evaluate(inputString).toString();
+        setInput(result);
+    };
 
     return (
         <View style={styles.container}>
@@ -11,12 +47,12 @@ export default function App() {
             It is used to avoid content from being blocked by notches, camera, and other elements that are located in the safe area. */}
             <SafeAreaView>
                 <TextInput
-                    maxLength={7}
+                    maxLength={15}
                     value={input}
                     keyboardType="number-pad"
-                    showSoftInputOnFocus={false}
                     style={styles.input}
                     editable={false}
+                    placeholder="0"
                 />
                 <View style={styles.row}>
                     <Button
@@ -51,8 +87,9 @@ export default function App() {
                         theme="primary"
                     />
                     <Button
-                        text="÷"
+                        text="/"
                         theme="primary"
+                        onPress={() => addOperator("/")}
                     />
                 </View>
                 <View style={styles.row}>
@@ -72,8 +109,9 @@ export default function App() {
                         onPress={() => setInput(prev => prev + "9")}
                     />
                     <Button
-                        text="x"
+                        text="*"
                         theme="primary"
+                        onPress={() => addOperator("*")}
                     />
                 </View>
                 <View style={styles.row}>
@@ -95,6 +133,7 @@ export default function App() {
                     <Button
                         text="-"
                         theme="primary"
+                        onPress={() => addOperator("-")}
                     />
                 </View>
                 <View style={styles.row}>
@@ -116,6 +155,7 @@ export default function App() {
                     <Button
                         text="+"
                         theme="primary"
+                        onPress={() => addOperator("+")}
                     />
                 </View>
                 <View style={styles.row}>
@@ -134,6 +174,7 @@ export default function App() {
                     <Button
                         text="="
                         theme="white"
+                        onPress={() => caculate()}
                     />
                 </View>
             </SafeAreaView>
@@ -157,7 +198,7 @@ const styles = StyleSheet.create({
     input: {
         width: "auto",
         height: "30%",
-        fontSize: 75,
+        fontSize: 50,
         textAlign: "right",
         color: "#91afb7",
         fontWeight: "400",
